2e9fd515b341c248c430179500f25a244e0bab0d
myke black <mykeb@hotmail.com>
2019-02-16 09:48:24 +0000

    Updates

---
+++ precache-manifest.044ae6fd379a0cc3c8014199a473ee47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f070974603936239e41",
+    "url": "/react-jobby/static/css/main.92d7e990.chunk.css"
+  },
+  {
+    "revision": "9f070974603936239e41",
+    "url": "/react-jobby/static/js/main.9f070974.chunk.js"
+  },
+  {
+    "revision": "0c5825e5ae0cc0a169bc",
+    "url": "/react-jobby/static/js/runtime~main.0c5825e5.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/react-jobby/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-jobby/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "144bbcf1decbc0af48e0b3df81608c06",
+    "url": "/react-jobby/index.html"
+  }
+];
